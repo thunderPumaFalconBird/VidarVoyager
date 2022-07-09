51018c955b762aa31b90a3ba90875502c54cffcf
Dave Schimdt <daveschimdt@Daves-Mac-Pro.local>
2022-07-09 11:28:17 -0500

    Initial commit

---
+++ android/src/com/vv/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.vv.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.vv.game.VidarVoyager;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VidarVoyager(), config);
+	}
+}
