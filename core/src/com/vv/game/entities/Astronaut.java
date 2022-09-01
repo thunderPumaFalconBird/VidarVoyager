8baf57885b5f295a8a6ea1954472e187700a3051
thunderPumaFalconBrid <thunderpumafalconbird@proton.me>
2022-08-31 22:45:18 -0500

    Added textures for minesweeper. Also added a spot for minesweeper in the level1 map.

--- core/src/com/vv/game/entities/Astronaut.java
+++ core/src/com/vv/game/entities/Astronaut.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.vv.game.VidarVoyager;
-
 import java.util.EnumMap;
 
 public class Astronaut  extends Actor {
