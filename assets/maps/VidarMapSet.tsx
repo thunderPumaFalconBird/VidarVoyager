79fa131a463062c16577e0c1b6fe88e09c356eee
thunderPumaFalconBrid <thunderpumafalconbird@proton.me>
2022-08-31 19:22:05 -0500

    Made the tile set for the map. I also made the first level. I haven't made the objects yet.

---
+++ assets/maps/VidarMapSet.tsx
@@ -0,0 +1,178 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="VidarMapSet" tilewidth="200" tileheight="200" tilecount="58" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="100" height="200" source="../../../Artwork/Vidar/MapIndividuals/8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="100" height="200" source="../../../Artwork/Vidar/MapIndividuals/9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackCornerBL.png"/>
+ </tile>
+ <tile id="10">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackCornerBR.png"/>
+ </tile>
+ <tile id="11">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackCornerTL.png"/>
+ </tile>
+ <tile id="12">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackCornerTR.png"/>
+ </tile>
+ <tile id="13">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackFloor1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackFloor2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackFloor3.png"/>
+ </tile>
+ <tile id="16">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackFloorArrowLeft.png"/>
+ </tile>
+ <tile id="17">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackFloorArrowRight.png"/>
+ </tile>
+ <tile id="18">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackFloorArrowUp.png"/>
+ </tile>
+ <tile id="19">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackFloorCorner.png"/>
+ </tile>
+ <tile id="20">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackWallBottom.png"/>
+ </tile>
+ <tile id="21">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackWallLeft.png"/>
+ </tile>
+ <tile id="22">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackWallRight.png"/>
+ </tile>
+ <tile id="23">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlackWallTop.png"/>
+ </tile>
+ <tile id="24">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/blueCornerBL.png"/>
+ </tile>
+ <tile id="25">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/blueCornerBR.png"/>
+ </tile>
+ <tile id="26">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/blueCornerTL.png"/>
+ </tile>
+ <tile id="27">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/blueCornerTR.png"/>
+ </tile>
+ <tile id="28">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueFloor2.png"/>
+ </tile>
+ <tile id="29">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueFloor3.png"/>
+ </tile>
+ <tile id="30">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueFloor4.png"/>
+ </tile>
+ <tile id="31">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueFloorHashPlay.png"/>
+ </tile>
+ <tile id="32">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueObject1.png"/>
+ </tile>
+ <tile id="33">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueObject2.png"/>
+ </tile>
+ <tile id="34">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueObject3.png"/>
+ </tile>
+ <tile id="35">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BlueScreen.png"/>
+ </tile>
+ <tile id="36">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/blueWallRight.png"/>
+ </tile>
+ <tile id="37">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownCornerBL.png"/>
+ </tile>
+ <tile id="38">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownCornerBR.png"/>
+ </tile>
+ <tile id="39">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownCOrnerTL.png"/>
+ </tile>
+ <tile id="40">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownCornerTR.png"/>
+ </tile>
+ <tile id="41">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownFloor.png"/>
+ </tile>
+ <tile id="42">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownFloor2.png"/>
+ </tile>
+ <tile id="43">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownFloor3.png"/>
+ </tile>
+ <tile id="44">
+  <image width="100" height="103" source="../../../Artwork/Vidar/MapIndividuals/BrownFloor4.png"/>
+ </tile>
+ <tile id="45">
+  <image width="100" height="103" source="../../../Artwork/Vidar/MapIndividuals/BrownObject1.png"/>
+ </tile>
+ <tile id="46">
+  <image width="100" height="103" source="../../../Artwork/Vidar/MapIndividuals/BrownObject2.png"/>
+ </tile>
+ <tile id="47">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownObject3.png"/>
+ </tile>
+ <tile id="48">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/BrownObject4.png"/>
+ </tile>
+ <tile id="49">
+  <image width="200" height="200" source="../../../Artwork/Vidar/MapIndividuals/BrownObject4tiles.png"/>
+ </tile>
+ <tile id="50">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/DarkBrownFloor2.png"/>
+ </tile>
+ <tile id="51">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/Door.png"/>
+ </tile>
+ <tile id="52">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/DoorCircle.png"/>
+ </tile>
+ <tile id="53">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/DoorPanels.png"/>
+ </tile>
+ <tile id="54">
+  <image width="100" height="103" source="../../../Artwork/Vidar/MapIndividuals/TanksSmall.png"/>
+ </tile>
+ <tile id="55">
+  <image width="100" height="100" source="../../../Artwork/Vidar/MapIndividuals/Untitled.png"/>
+ </tile>
+ <tile id="56">
+  <image width="200" height="100" source="../../../Artwork/Vidar/MapIndividuals/doorLeft.png"/>
+ </tile>
+ <tile id="57">
+  <image width="200" height="200" source="../../../Artwork/Vidar/MapIndividuals/door12.png"/>
+ </tile>
+</tileset>
