5474dc7debb88a0121508f5daae9fdd44d6d3c35
thunderPumaFalconBrid <thunderpumafalconbird@proton.me>
2022-09-10 09:21:46 -0500

    Finished initAnimations.

--- core/src/com/vv/game/entities/Astronaut.java
+++ core/src/com/vv/game/entities/Astronaut.java
@@ -2,6 +2,7 @@ package com.vv.game.entities;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -19,10 +20,6 @@ public class Astronaut  extends Actor {
         walkingBack,
         walkingSideLeft,
         walkingSideRight,
-        runningFront,
-        runningBack,
-        runningSideLeft,
-        RunningSideRight,
         stalled
     }
     private EnumMap<STATE, Animation<TextureRegion>> animations;
@@ -65,6 +62,9 @@ public class Astronaut  extends Actor {
             stateTime += deltaTime;
             currentFrame = animations.get(currentState).getKeyFrame(stateTime, true);
         }
+        else if(previousState == STATE.stalled){
+            stateTime = 0; // TODO this will need to be changed.
+        }
     }
 
     @Override
@@ -83,11 +83,44 @@ public class Astronaut  extends Actor {
 
     private void initAnimations(){
         animations = new EnumMap<>(STATE.class);
+        TextureAtlas textureAtlas = new TextureAtlas("astronaut/Astronaut.txt");
 
-        Array<TextureRegion> temp = new Array<>();
+        Array<TextureRegion> atlasTemp = new Array<TextureRegion>(textureAtlas.getRegions());
+        Array<TextureRegion> animationTemp = new Array<>();
 
-        //TODO add textureRegions
-        //TODO set currentFrame to idleFront
+        //ADDING TEXTURE REGIONS TO ANIMATIONS ENUM MAP
 
+        // adding Side facing idle
+        animationTemp.addAll(atlasTemp, 0, 10);
+        animations.put(STATE.idleSide, new Animation<>(.2f, animationTemp));
+        animationTemp.clear();
+
+        // adding Back facing idle
+        animationTemp.addAll(atlasTemp, 10, 9);
+        animations.put(STATE.idleBack, new Animation<>(.2f, animationTemp));
+        animationTemp.clear();
+
+        // adding Front facing idle
+        animationTemp.addAll(atlasTemp, 19, 10);
+        animations.put(STATE.idleFront, new Animation<>(.2f, animationTemp));
+        animationTemp.clear();
+
+        // adding Side facing Walking
+        animationTemp.addAll(atlasTemp, 29, 12);
+        animations.put(STATE.walkingSideLeft, new Animation<>(.2f, animationTemp));
+        animationTemp.clear();
+
+        // adding Back facing Walking
+        animationTemp.addAll(atlasTemp, 41, 12);
+        animations.put(STATE.walkingBack, new Animation<>(.2f, animationTemp));
+        animationTemp.clear();
+
+        // adding Front facing Walking
+        animationTemp.addAll(atlasTemp, 53, 12);
+        animations.put(STATE.walkingFront, new Animation<>(.2f, animationTemp));
+        animationTemp.clear();
+
+        //SET CURRENT FRAME TO CURRENT STATE
+        currentFrame.setRegion(animations.get(currentState).getKeyFrame(stateTime, true));
     }
 }
